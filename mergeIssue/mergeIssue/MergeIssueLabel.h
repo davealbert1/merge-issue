5ebf3bf66a6945be7c729f0e76bb230b0cb14faf
Andrew Caulfield <andrewjcaulfield@gmail.com>
2013-09-18 15:15:27 +0100

    Added lots of files.

---
+++ mergeIssue/mergeIssue/MergeIssueLabel.h
@@ -0,0 +1,13 @@
+//
+//  MergeIssueLabel.h
+//  mergeIssue
+//
+//  Created by Andrew Caulfield on 18/09/2013.
+//  Copyright (c) 2013 Dave Albert. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MergeIssueLabel : UILabel
+
+@end
---
+++ mergeIssue/mergeIssue/MergeIssueLabel.m
@@ -0,0 +1,31 @@
+//
+//  MergeIssueLabel.m
+//  mergeIssue
+//
+//  Created by Andrew Caulfield on 18/09/2013.
+//  Copyright (c) 2013 Dave Albert. All rights reserved.
+//
+
+#import "MergeIssueLabel.h"
+
+@implementation MergeIssueLabel
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
